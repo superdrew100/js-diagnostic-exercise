3b6803183e7a4f8111280268f4cfc8068661b73b
Drew Thomasson <superdrew100@Gmail.com>
2020-04-07 13:23:51 -0400

    I think im starting to figure out how to edit the innertext value

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("Hello from app.js")
+//const 
+
+
+//if (click checking deposit){
+//add moneyamount value to checking
+//}
+
+//if (click checking withdraw )
+
+
+//when you click the deposit button you should add the 
+//value in the field to the current balance
+
+//This is creating the objects for the buttons
+const checkingDepositButton = document.getElementById('checkingDepositButton')
+const checkingWithdrawBUtton = document.getElementById('checkingWithdrawButton')
+const savingsDespoitButton = document.getElementById('savingsDepositButton')
+const savingsWithdrawButton = document.getElementById('savingsWithdrawButton')
+const checkingBalance = document.getElementById('checkingBalance')
+const savingsBalance = document.getElementById('savingsBalance')
+const checkingInput = document.getElementById('checkingInput')
+const savingsInput = document.getElementById('savingsInput')
+
+savingsDespoitButton.addEventListener('click', function() {
+    alert("gangasta")
+    //add .value to balance.innerText
+})
